311cbf40494227b1d5f3ece4565ca03a60d5efa3
Mariusz Usnarski <mariusz.usnarski@gmail.com>
2019-08-22 18:01:09 +0200

    Add basic styles and javascript variables

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Hello World");
+
+    const rootElement = document.querySelector('#root');
+    const sections = document.querySelectorAll('section')
+
+    document.addEventListener('mousewheel', function (event) {
+        console.log(event.wheelDelta)
+    })
+})
